d27f8de8f8ab4f676d33dfbea964151607c0690b
kishanga <85829332+kishanga@users.noreply.github.com>
2024-09-09 21:25:48 +0800

    Update streamlitdeploy.py

--- streamlitdeploy.py
+++ streamlitdeploy.py
@@ -12,44 +12,14 @@ Default data is loaded to showcase the forecast based on historical data.
 Select a company to view forecasts and historical data.
 """)
 
-# Define model paths
+# Define model paths and historical data paths
 model_paths = {
-    "Meta Scope 1": 'model/meta_scope1_model',
-    "Meta Scope 2": 'model/meta_scope2_model',
-    "Meta Scope 3": 'model/meta_scope3_model',
-    "Fujitsu Scope 1": 'model/fujitsu_scope1_model',
-    "Fujitsu Scope 2": 'model/fujitsu_scope2_model',
-    "Fujitsu Scope 3": 'model/fujitsu_scope3_model',
-    "Amazon Scope 1": 'model/amazon_scope1_model',
-    "Amazon Scope 2": 'model/amazon_scope2_model',
-    "Amazon Scope 3": 'model/amazon_scope3_model',
-    "Google Scope 1": 'model/google_scope1_model',
-    "Google Scope 2": 'model/google_scope2_model',
-    "Google Scope 3": 'model/google_scope3_model',
-    "Microsoft Scope 1": 'model/microsoft_scope1_model',
-    "Microsoft Scope 2": 'model/microsoft_scope2_model',
-    "Microsoft Scope 3": 'model/microsoft_scope3_model'
+    # Define your model paths here
 }
 
-# Define historical data paths
 historical_data_paths = {
-    "Meta Scope 1": 'data/meta_scope1.csv',
-    "Meta Scope 2": 'data/meta_scope2.csv',
-    "Meta Scope 3": 'data/meta_scope3.csv',
-    "Fujitsu Scope 1": 'data/fujitsu_scope1.csv',
-    "Fujitsu Scope 2": 'data/fujitsu_scope2.csv',
-    "Fujitsu Scope 3": 'data/fujitsu_scope3.csv',
-    "Amazon Scope 1": 'data/amazon_scope1.csv',
-    "Amazon Scope 2": 'data/amazon_scope2.csv',
-    "Amazon Scope 3": 'data/amazon_scope3.csv',
-    "Google Scope 1": 'data/google_scope1.csv',
-    "Google Scope 2": 'data/google_scope2.csv',
-    "Google Scope 3": 'data/google_scope3.csv',
-    "Microsoft Scope 1": 'data/microsoft_scope1.csv',
-    "Microsoft Scope 2": 'data/microsoft_scope2.csv',
-    "Microsoft Scope 3": 'data/microsoft_scope3.csv'
+    # Define your historical data paths here
 }
-
 
 # Load models
 def load_models(model_paths):
@@ -79,7 +49,7 @@ historical_data = load_historical_data(historical_data_paths)
 def combine_data(historical, prediction, label, custom_name=None, uploaded_file=False):
     pred_index = pd.date_range(start=historical.index[-1] + pd.DateOffset(1), periods=len(prediction), freq='Y')
     prediction_series = pd.Series(prediction, index=pred_index, name=f'Prediction {label}')
-
+    
     if uploaded_file:
         # Use custom name for the uploaded file
         original_label = custom_name if custom_name else f'{label} Original'
@@ -89,20 +59,17 @@ def combine_data(historical, prediction, label, custom_name=None, uploaded_file=
         # Handle the case with default data
         combined_data = pd.concat([historical, prediction_series], axis=0)
         combined_data.columns = [f'{label} Original', f'{label} Prediction']
-
+    
     return combined_data
 
-
-# Streamlit App
+# Streamlit App Title
 st.title('''Carbon Emission Predictor
-This app is built base on this five tech companies historical emission data for the past 6 years. 
+This app is built based on five tech companies' historical emission data for the past 6 years. 
 
-(Meta, Fujitsu, Amazon, Google and Microsoft)''')
+(Meta, Fujitsu, Amazon, Google, and Microsoft)''')
 
 # User input for company and year
 company = st.sidebar.selectbox('Select a company:', ["Meta", "Fujitsu", "Amazon", "Google", "Microsoft"], index=0)
-
-
 uploaded_file = st.sidebar.file_uploader("Upload your CSV file for comparison", type=["csv"])
 
 if uploaded_file is not None:
@@ -122,15 +89,15 @@ if uploaded_file is not None:
 
     if model_scope_1 in models:
         predictions_scope_1 = predict_model(models[model_scope_1], fh=fh)
-        combined_data_scope_1 = combine_data(user_data, predictions_scope_1.values.flatten(), model_scope_1, custom_name = original_label)
+        combined_data_scope_1 = combine_data(user_data, predictions_scope_1.values.flatten(), model_scope_1, custom_name=original_label, uploaded_file=True)
 
     if model_scope_2 in models:
         predictions_scope_2 = predict_model(models[model_scope_2], fh=fh)
-        combined_data_scope_2 = combine_data(user_data, predictions_scope_2.values.flatten(), model_scope_2, custom_name = original_label)
+        combined_data_scope_2 = combine_data(user_data, predictions_scope_2.values.flatten(), model_scope_2, custom_name=original_label, uploaded_file=True)
 
     if model_scope_3 in models:
         predictions_scope_3 = predict_model(models[model_scope_3], fh=fh)
-        combined_data_scope_3 = combine_data(user_data, predictions_scope_3.values.flatten(), model_scope_3, custom_name = original_label)
+        combined_data_scope_3 = combine_data(user_data, predictions_scope_3.values.flatten(), model_scope_3, custom_name=original_label, uploaded_file=True)
 
     # Combine all scopes into a single DataFrame for plotting
     final_combined_data = pd.concat([combined_data_scope_1, combined_data_scope_2, combined_data_scope_3], axis=1)
@@ -138,81 +105,29 @@ if uploaded_file is not None:
 else:
     # Default historical data if no file is uploaded
     user_data = historical_data[f"{company} Scope 1"]
-    
+
 # Tabs for Combined Charts, Individual Scope Charts, and Emission Stats Table
 tab1, tab2, tab3 = st.tabs(["Combined Charts", "Individual Scope Charts", "Emission Stats Table (in metrics tons)"])
 
-# Get the relevant model names for the selected company
-model_names = [f"{company} Scope 1", f"{company} Scope 2", f"{company} Scope 3"]
-
-combined_data_list = []
-
-for scope in model_names:
-    if scope in models:
-        predictions = predict_model(models[scope], fh=30)
-        combined_data = combine_data(user_data, predictions.values.flatten(), scope)
-        combined_data_list.append(combined_data)
-
-# Combine all scopes into a single DataFrame for plotting
-final_combined_data = pd.concat(combined_data_list, axis=1)
-
-# Combined Charts Tab
 # Combined Charts Tab
 with tab1:
-    st.subheader('Carbon Emissions Comparison: Scopes 1, 2, and 3 (Original vs Predictions)')
+    st.subheader(f'{company}: Scopes 1, 2, and 3 (Original vs Predictions)')
 
-    # Multi-select widget to choose companies for comparison
-    companies_to_compare = st.multiselect('Compare with:', ["Meta", "Fujitsu", "Amazon", "Google", "Microsoft"], key='company_comparison')
-
-    if companies_to_compare:
-        combined_data_list = []
-
-        for company in companies_to_compare:
-            model_names = [f"{company} Scope 1", f"{company} Scope 2", f"{company} Scope 3"]
-
-            for scope in model_names:
-                if scope in models:
-                    predictions = predict_model(models[scope], fh=30)
-                    # Use the user_data if uploaded, otherwise, use historical data
-                    historical_scope_data = user_data if uploaded_file else historical_data[scope]
-                    combined_data = combine_data(historical_scope_data, predictions.values.flatten(), scope, custom_name=original_label, uploaded_file=uploaded_file)
-                    combined_data_list.append(combined_data)
-
-        if combined_data_list:
-            final_combined_data = pd.concat(combined_data_list, axis=1)
-
-            # Chart Type Selection
-            chart_type = st.selectbox('Select chart type:', ['Line', 'Bar', 'Scatter'], key='chart_type_selection_combined')
-
-            # Chart rendering logic
-            if chart_type == 'Line':
-                fig_combined = px.line(final_combined_data, 
-                                       x=final_combined_data.index, 
-                                       y=final_combined_data.columns, 
-                                       title=f'{company}: Scopes 1, 2, and 3 (Original vs Predictions)', 
-                                       labels={"index": "Year", "value": "Emissions (in metric tons)"})
-            elif chart_type == 'Bar':
-                fig_combined = px.bar(final_combined_data, 
-                                      x=final_combined_data.index, 
-                                      y=final_combined_data.columns, 
-                                      title=f'{company}: Scopes 1, 2, and 3 (Original vs Predictions)', 
-                                      labels={"index": "Year", "value": "Emissions (in metric tons)"})
-            elif chart_type == 'Scatter':
-                fig_combined = px.scatter(final_combined_data, 
-                                          x=final_combined_data.index, 
-                                          y=final_combined_data.columns, 
-                                          title=f'{company}: Scopes 1, 2, and 3 (Original vs Predictions)', 
-                                          labels={"index": "Year", "value": "Emissions (in metric tons)"})
-
-            st.plotly_chart(fig_combined)
-    else:
-        # Default data display when no company selected
+    if uploaded_file:
+        # Display a dynamic title using the uploaded file name
         fig_combined = px.line(final_combined_data, 
                                x=final_combined_data.index, 
                                y=final_combined_data.columns, 
-                               title=f'{company} : Scopes 1, 2, and 3 (Original vs Predictions)', 
+                               title=f'{original_label}: Scopes 1, 2, and 3 (Original vs Predictions)', 
                                labels={"index": "Year", "value": "Emissions (in metric tons)"})
-        st.plotly_chart(fig_combined)
+    else:
+        fig_combined = px.line(final_combined_data, 
+                               x=final_combined_data.index, 
+                               y=final_combined_data.columns, 
+                               title=f'{company}: Scopes 1, 2, and 3 (Original vs Predictions)', 
+                               labels={"index": "Year", "value": "Emissions (in metric tons)"})
+
+    st.plotly_chart(fig_combined)
 
 
 # Individual Scope Charts Tab
