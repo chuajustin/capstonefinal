1ef303a62c0f75a6cce038f21e442b2933f48ae1
Chua Justin <chuajustin@hotmail.com>
2024-09-10 18:54:50 +0800

    Update streamlitdeploy.py

--- streamlitdeploy.py
+++ streamlitdeploy.py
@@ -103,6 +103,7 @@ if uploaded_file is not None:
             'Scope 2': models.get('Meta Scope 2', None),
             'Scope 3': models.get('Meta Scope 3', None)
         }
+        st.write(selected_models)
 
         # Check if the models are available and make predictions
         combined_user_data = pd.DataFrame()
