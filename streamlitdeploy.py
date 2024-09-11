3147dd2d5402eae721c1942117bfb3b5ae07d038
Chua Justin <chuajustin@hotmail.com>
2024-09-11 18:36:50 +0800

    Update streamlitdeploy.py

--- streamlitdeploy.py
+++ streamlitdeploy.py
@@ -214,8 +214,8 @@ with tab2:
                 # In the second column, display the forecast values for 2030 and 2050
                 with col2:
                     try:
-                        forecast_2030 = comparison_data.loc[2030-12-31 00:00:00] if 2030-12-31 00:00:00 in comparison_data.index else "2030 data not available"
-                        forecast_2050 = comparison_data.loc[2050-12-31 00:00:00] if 2050-12-31 00:00:00 in comparison_data.index else "2050 data not available"
+                        forecast_2030 = comparison_data.loc['2030-12-31 00:00:00'] if '2030-12-31 00:00:00' in comparison_data.index else "2030 data not available"
+                        forecast_2050 = comparison_data.loc['2050-12-31 00:00:00'] if '2050-12-31 00:00:00' in comparison_data.index else "2050 data not available"
                         st.write(f"### {scope} Forecast")
                         st.write(f"- **2030 Forecast**: {forecast_2030}")
                         st.write(f"- **2050 Forecast**: {forecast_2050}")
