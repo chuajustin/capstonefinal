c680c403141fb776a7d1e3d2189c6a5e4cd7a121
Chua Justin <chuajustin@hotmail.com>
2024-09-11 18:41:43 +0800

    Update streamlitdeploy.py

--- streamlitdeploy.py
+++ streamlitdeploy.py
@@ -214,12 +214,11 @@ with tab2:
                 # In the second column, display the forecast values for 2030 and 2050
                 with col2:
                     try:
-                        forecast_2030 = comparison_data.loc['2030-12-31 00:00:00'] if '2030-12-31 00:00:00' in comparison_data.index else "2030 data not available"
-                        forecast_2050 = comparison_data.loc['2050-12-31 00:00:00'] if '2050-12-31 00:00:00' in comparison_data.index else "2050 data not available"
+                        forecast_2030 = predictions.loc['2030-12-31 00:00:00'] if '2030-12-31 00:00:00' in comparison_data.index else "2030 data not available"
+                        forecast_2050 = predictions.loc['2050-12-31 00:00:00'] if '2050-12-31 00:00:00' in comparison_data.index else "2050 data not available"
                         st.write(f"### {scope} Forecast")
                         st.write(f"- **2030 Forecast**: {forecast_2030}")
                         st.write(f"- **2050 Forecast**: {forecast_2050}")
-                        st.write(final_combined_data.index)
                     except Exception as e:
                         st.write("Error fetching forecast data:", e)
 
