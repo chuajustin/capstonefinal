7ebffcbaa0887d990eed5177741dfc248ce7d09e
Chua Justin <chuajustin@hotmail.com>
2024-09-11 21:01:43 +0800

    Update streamlitdeploy.py

--- streamlitdeploy.py
+++ streamlitdeploy.py
@@ -218,9 +218,8 @@ with tab2:
                     try:
 
                         # Retrieve the forecast values for the current scope
-                        forecast_2030 = predictions.loc['2030', scope].values.flatten() if '2030' in predictions.index else "2030 data not available"
-                        forecast_2050 = predictions.loc['2050', scope].values.flatten() if '2050' in predictions.index else "2050 data not available"
-                        
+                        forecast_2030 = predictions.loc['2030'].values.flatten() if '2030' in predictions.index else "2030 data not available"
+                        forecast_2050 = predictions.loc['2050'].values.flatten() if '2050' in predictions.index else "2050 data not available"
                         st.write(f"### {scope} Forecast")
                         st.write(f"- **2030 Forecast**: {forecast_2030}")
                         st.write(f"- **2050 Forecast**: {forecast_2050}")
@@ -253,8 +252,8 @@ with tab2:
                 with col2:
                     try:
                         # Retrieve the forecast values for the current scope
-                        forecast_2030 = predictions.loc['2030', scope].values.flatten() if '2030' in predictions.index else "2030 data not available"
-                        forecast_2050 = predictions.loc['2050', scope].values.flatten() if '2050' in predictions.index else "2050 data not available"
+                        forecast_2030 = predictions.loc['2030'].values.flatten() if '2030' in predictions.index else "2030 data not available"
+                        forecast_2050 = predictions.loc['2050'].values.flatten() if '2050' in predictions.index else "2050 data not available"
                         st.write(f"- **2030 Forecast**: {forecast_2030}")
                         st.write(f"- **2050 Forecast**: {forecast_2050}")
                     
