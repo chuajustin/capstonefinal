f7174e29174ec034e50c0cc696a118fd4eac4b98
Chua Justin <chuajustin@hotmail.com>
2024-09-11 22:10:24 +0800

    Update streamlitdeploy.py

--- streamlitdeploy.py
+++ streamlitdeploy.py
@@ -251,8 +251,8 @@ with tab2:
                 with col2:
                     try:
                         # Retrieve the forecast values for the current scope
-                        forecast_2030 = predictions.loc['2030'].values.flatten() if '2030' in final_combined_data.index else "2030 data not available"
-                        forecast_2050 = predictions.loc['2050'].values.flatten() if '2050' in final_combined_data.index else "2050 data not available"
+                        forecast_2030 = predictions.loc['2030'].values.flatten() if '2030' in predictions.index else "2030 data not available"
+                        forecast_2050 = predictions.loc['2050'].values.flatten() if '2050' in predictions.index else "2050 data not available"
                         st.write(f"- **2030 Forecast**: {forecast_2030}")
                         st.write(f"- **2050 Forecast**: {forecast_2050}")
                     
