1a2a98682c87c0c497cc7892111eb7cc04f90b34
kishanga <85829332+kishanga@users.noreply.github.com>
2024-09-13 12:39:38 +0800

    Update streamlitdeploy.py

--- streamlitdeploy.py
+++ streamlitdeploy.py
@@ -138,11 +138,11 @@ if user_data is not None:
     predictions_combined_data_list = pd.Series(predictions_combined_data_list)
     combined_data_list.append(predictions_combined_data_list) 
 
-    st.write(combined_data_list)
+    st.DataFrame(combined_data_list)
 
     # Combine all scopes into a single DataFrame for plotting
     #final_combined_data = pd.DataFrame(combined_data_list)
-    final_combined_data = pd.concat(combined_data_list, axis=1)
+    #final_combined_data = pd.concat(combined_data_list, axis=1)
 
 
 # Combined Charts Tab
